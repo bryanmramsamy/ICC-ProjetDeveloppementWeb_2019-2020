adb6dd206abd9346b7acd7a2088ce5f1fef237b8
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-04-17 00:55:19 +0200

    Clean comments of previous methods

--- public/projet-web/models/Manager.php
+++ public/projet-web/models/Manager.php
@@ -12,17 +12,8 @@ class Manager {
         return $db;
     }
 
-
-
-
- 
-
-
-
-
-
     # Calcule nombre de message par page et appelle l'execution de la requête
-    public function getEntries_byPage($db_table, $query, $page, $nb_message_per_page){
+    protected function getEntries_byPage($db_table, $query, $page, $nb_message_per_page){
         $actual_page = $this->getActualPage($db_table, $page, $nb_message_per_page);
         
         $offset = $nb_message_per_page * ($actual_page - 1);
@@ -32,7 +23,7 @@ class Manager {
     }
 
     # Vérifie si la page passé en paramètre est valide
-    public function getActualPage($db_table, $page, $nb_message_per_page){
+    protected function getActualPage($db_table, $page, $nb_message_per_page){
         $total_pages = $this->getTotalPages($db_table, $nb_message_per_page);
 
         if ($page < 1) {
@@ -45,12 +36,12 @@ class Manager {
     }
 
     # Compte le nombre de pages total selon le nombre de nombre de message
-    public function getTotalPages($db_table, $nb_message_per_page){
+    protected function getTotalPages($db_table, $nb_message_per_page){
         $nb_message = $this->getNumberEntries($db_table);
         return intdiv($nb_message, $nb_message_per_page) + 1;
     }
 
-    public function getNumberEntries($db_table){
+    private function getNumberEntries($db_table){
         $db = $this->dbConnect();
 
         $req = $db->query('SELECT COUNT(*) AS count_entries FROM ' . $db_table);
@@ -62,14 +53,11 @@ class Manager {
     }
 
     # Effectue la requête en utilisant offset en row_count calculé dans getMessage_byPage
-    public function getEntries_byRange($query, $offset, $row_count){
+    protected function getEntries_byRange($query, $offset, $row_count){
         $db = $this->dbConnect();
-
-        // $req = $db->query('SELECT minichat.*, users.id, users.username, users.last_name, users.first_name FROM minichat INNER JOIN users ON minichat.userID = users.id ORDER BY date_edition DESC LIMIT ' . $offset . ',' . $row_count);
 
         $req = $db->query($query . ' LIMIT ' . $offset . ',' . $row_count);
 
         return $req;
     }
-
 }
