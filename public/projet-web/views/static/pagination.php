84bb2b993f82e9601b246e4a7d53ade969d8fbd3
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-04-29 21:26:58 +0200

    Add pagination on top of the posts page

--- public/projet-web/views/static/pagination.php
+++ public/projet-web/views/static/pagination.php
@@ -3,29 +3,31 @@ $previous_page = $actual_page - 1;
 $next_page = $actual_page + 1;
 ?>
 
-<nav aria-label="Page navigation example">
-    <ul class="pagination">
+<div class="d-flex justify-content-center">
+    <nav aria-label="Page navigation example">
+        <ul class="pagination">
 
-        <?php
-        $pre_pagination_action = $_GET['action'];
+            <?php
+            $pre_pagination_action = $_GET['action'];
 
-        if ($actual_page > 1) {
-        ?>
-            <li class='page-item'><a class='page-link' href='index.php?action=<?= $pre_pagination_action; ?>&page=1'>Première page</a></li>
+            if ($actual_page > 1) {
+            ?>
+                <li class='page-item'><a class='page-link' href='index.php?action=<?= $pre_pagination_action; ?>&page=1'>Première page</a></li>
 
-            <li class='page-item'><a class='page-link' href='index.php?action=<?= $pre_pagination_action; ?>&page=<?= $previous_page; ?>'>Page précédente</a></li>
-        <?php } ?>
+                <li class='page-item'><a class='page-link' href='index.php?action=<?= $pre_pagination_action; ?>&page=<?= $previous_page; ?>'>Page précédente</a></li>
+            <?php } ?>
 
-        <li class="page-item active" aria-current="page">
-            <span class="page-link">Page <?= $actual_page; ?> sur <?= $total_pages; ?><span class="sr-only">(current)</span>
-            </span>
-        </li>
+            <li class="page-item active" aria-current="page">
+                <span class="page-link">Page <?= $actual_page; ?> sur <?= $total_pages; ?><span class="sr-only">(current)</span>
+                </span>
+            </li>
 
-        <?php if ($actual_page < $total_pages) { ?>
-            <li class='page-item'><a class='page-link' href='index.php?action=<?= $pre_pagination_action; ?>&page=<?= $next_page; ?>'>Page suivante</a></li>
+            <?php if ($actual_page < $total_pages) { ?>
+                <li class='page-item'><a class='page-link' href='index.php?action=<?= $pre_pagination_action; ?>&page=<?= $next_page; ?>'>Page suivante</a></li>
 
-            <li class='page-item'><a class='page-link' href='index.php?action=<?= $pre_pagination_action; ?>&page=<?= $total_pages; ?>'>Dernière page</a></li>
-        <?php } ?>
+                <li class='page-item'><a class='page-link' href='index.php?action=<?= $pre_pagination_action; ?>&page=<?= $total_pages; ?>'>Dernière page</a></li>
+            <?php } ?>
 
-    </ul>
-</nav>
+        </ul>
+    </nav>
+</div>
