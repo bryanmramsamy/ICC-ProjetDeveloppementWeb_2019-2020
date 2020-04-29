5147b8fb57a596dce0f7cfa1279ea57e9aa6982e
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-04-29 20:39:38 +0200

    Add Bootstrap to header and login-area

--- public/projet-web/views/authentication/signin.php
+++ public/projet-web/views/authentication/signin.php
@@ -1,35 +1,36 @@
 <?php if (isset($_SESSION['userID']) && !empty($_SESSION['userID'])) { ?>
-    
-    <p>
-        Bonjour <?= displayed_name($_SESSION['username'], $_SESSION['user_first_name'], $_SESSION['user_last_name']); ?>
-        <br />
-        <a class="btn btn-primary" href="index.php?action=profile">Voir votre profile</a> 
-        <a class="btn btn-secondary" href="index.php?action=signout">Déconnexion</a>
-    </p>
+
+    <div class="d-flex">
+        <div class="mr-auto p-2 h3 text-primary">Bonjour <?= displayed_name($_SESSION['username'], $_SESSION['user_first_name'], $_SESSION['user_last_name']); ?></div>
+        <div class="p-2"><a class="btn btn-primary" href="index.php?action=profile">Voir votre profile</a> </div>
+        <div class="p-2"><a class="btn btn-secondary" href="index.php?action=signout">Déconnexion</a></div>
+    </div>
 
 <?php } else { ?>
-    
-    <form action="index.php?action=signin_post" method="post">
+    <div>
+        <form action="index.php?action=signin_post" method="post">
+            <div class="d-flex">
+                <div class="form-group form-inline mr-auto p-2">
+                    <label for='username'>Pseudonyme : </label>
+                    <input class="form-control" type='text' id='username' name='username' />
+                </div>
 
-        <div>
-            <label for='username'>Pseudonyme : </label>
-            <input type='text' id='username' name='username' />
-        </div>
+                <div class="form-group form-inline mr-auto p-2">
+                    <label for='password'>Mot de passe : </label>
+                    <input class="form-control" type='password' id='password' name='password' />
+                </div>
 
-        <div>
-            <label for='password'>Mot de passe : </label>
-            <input type='password' id='password' name='password' />
-        </div>
+                <div class="p-2">
+                    <input class="btn btn-primary" type=submit value="Se connecter" /> 
+                </div>
 
-        <div>
-            <input class="btn btn-primary" type=submit value="Se connecter" /> 
-            <a class="btn btn-secondary" href="index.php?action=register">Créer un compte</a>
-        </div>
-
-    </form>
+                <div class="p-2">
+                    <a class="btn btn-secondary" href="index.php?action=register">Créer un compte</a>
+                </div>
+            </div>
+        </form>
+    </div>
 
 <?php
 }
-
 ?>
-
--- public/projet-web/views/static/base.php
+++ public/projet-web/views/static/base.php
@@ -1,22 +1,17 @@
 <!doctype html>
 <html lang="en">
     <head>
-
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <title><?= $title ?></title>
-
     </head>
 
     <body>
-
         <div class="container">
-
             <header>
                 <a href="index.php">
-                    <div class="text-center border border-primary">
+                    <div class="text-center text-white bg-secondary rounded">
                         <h1>ICC-2020 Web Project</h1>
                         <h2>Bienvenue sur le site de vente de matériel et de documentation informatique !</h2>
                     </div>
