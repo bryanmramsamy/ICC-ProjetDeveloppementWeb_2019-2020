dd5c76a06b774300019752b57e2349e273119b2e
Bryan M. Ramsamy <rainbow_delta@outlook.com>
2020-05-23 01:13:07 +0200

    Add Bootstrap to basket tables

--- public/projet-web/views/shop/basket.php
+++ public/projet-web/views/shop/basket.php
@@ -12,18 +12,23 @@ ob_start();
         <a class="btn btn-primary btn-lg btn-block" href="index.php?action=shop">Visiter notre boutique</a>
     <?php } else { ?>
         <div>
-            <table style="width:100%; border-style: solid;">
-                <tr>
-                    <th>Numéro de commande</th>
-                    <th>Utilisateur</th>
-                    <th>Nombre total d'article</th>
-                    <th>Prix total de la commande</th>
-                </tr>
-                    <td>XXXX-XXXX<?= $order['id']; ?></td>
-                    <td><?= $_SESSION['username']; ?></td>
-                    <td><?= $number_items ?></td>
-                    <td>€ <?= number_format((float)$order['total'], 2, ',', ''); ?></td>
-                </tr>
+            <table class="table table-bordered table-striped">
+                <thead class="table-primary">
+                    <tr>
+                        <th>Numéro de commande</th>
+                        <th>Utilisateur</th>
+                        <th>Nombre total d'article</th>
+                        <th>Prix total de la commande</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>XXXX-XXXX<?= $order['id']; ?></td>
+                        <td><?= $_SESSION['username']; ?></td>
+                        <td><?= $number_items ?></td>
+                        <td>€ <?= number_format((float)$order['total'], 2, ',', ''); ?></td>
+                    </tr>
+                </tbody>
             </table> 
         </div>
 
@@ -33,23 +38,27 @@ ob_start();
 
         <br>
 
-        <table style="width:100%; border-style: solid;">
-            <tr>
-                <th>Nom de l'article</th>
-                <th>Quantité</th>
-                <th>Prix unitaire</th>
-                <th>Prix total</th>
-                <th></th>
-            </tr>
-            <?php while ($purchase = $purchases->fetch()) { ?>
+        <table class="table table-sm table-hover table-bordered table-striped">
+            <thead class="table-primary">
                 <tr>
-                    <td><?= $purchase['name']; ?></td>
-                    <td><?= $purchase['quantity']; ?></td>
-                    <td>€ <?= number_format((float)$purchase['unit_price'], 2, ',', ''); ?></td>
-                    <td>€ <?= number_format((float)$purchase['total_price'], 2, ',', ''); ?></td>
-                    <td><a class="btn btn-warning" href="index.php?action=shop_remove_from_basket_post&purchaseID=<?= $purchase['purchaseID']; ?>">Supprimer l'entrée</a></td>
+                    <th>Nom de l'article</th>
+                    <th>Quantité</th>
+                    <th>Prix unitaire</th>
+                    <th>Prix total</th>
+                    <th></th>
                 </tr>
-            <?php } ?>
+            </thead>
+            <tbody>
+                <?php while ($purchase = $purchases->fetch()) { ?>
+                    <tr>
+                        <td><?= $purchase['name']; ?></td>
+                        <td><?= $purchase['quantity']; ?></td>
+                        <td>€ <?= number_format((float)$purchase['unit_price'], 2, ',', ''); ?></td>
+                        <td>€ <?= number_format((float)$purchase['total_price'], 2, ',', ''); ?></td>
+                        <td><a class="btn btn-warning" href="index.php?action=shop_remove_from_basket_post&purchaseID=<?= $purchase['purchaseID']; ?>">Supprimer l'entrée</a></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
         </table> 
     <?php } ?>
 
